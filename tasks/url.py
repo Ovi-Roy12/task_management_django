459356f0f0cc7d9be68cbcdb35b6369802bb7532
unknown <aovroy44@gmail.com>
2025-01-11 15:49:26 +0600

    added about page

--- tasks/url.py
+++ tasks/url.py
@@ -4,5 +4,6 @@ from .import views
 app_name = 'tasks'
 urlpatterns =[
     path('',views.home, name='home'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to our Task Management System");
+    return HttpResponse("Welcome to our Task Management System")
 
 def contact(request):
-    return HttpResponse("This is Contact page of Task Management System");
+    return HttpResponse("This is Contact page of Task Management System")
+
+def about(request):
+    return HttpResponse("This is the About Page of Task Management System")
